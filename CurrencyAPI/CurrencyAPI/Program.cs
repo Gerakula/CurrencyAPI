7c68060b7e7597147f9942a50adfdae19a0d1e8a
Gerakula <gerakula56@gmail.com>
2023-09-23 19:28:41 +0300

    Create http request skeleton and json deserealization

--- CurrencyAPI/CurrencyAPI/Program.cs
+++ CurrencyAPI/CurrencyAPI/Program.cs
@@ -1,17 +1,82 @@
+using System.Text.Json;
+
 namespace CurrencyAPI
 {
+    public class Bpi
+    {
+        public USD USD { get; set; }
+        public GBP GBP { get; set; }
+        public EUR EUR { get; set; }
+    }
+
+    public class EUR
+    {
+        public string code { get; set; }
+        public string symbol { get; set; }
+        public string rate { get; set; }
+        public string description { get; set; }
+        public double rate_float { get; set; }
+    }
+
+    public class GBP
+    {
+        public string code { get; set; }
+        public string symbol { get; set; }
+        public string rate { get; set; }
+        public string description { get; set; }
+        public double rate_float { get; set; }
+    }
+
+    public class Root
+    {
+        public Time time { get; set; }
+        public string disclaimer { get; set; }
+        public string chartName { get; set; }
+        public Bpi bpi { get; set; }
+    }
+
+    public class Time
+    {
+        public string updated { get; set; }
+        public DateTime updatedISO { get; set; }
+        public string updateduk { get; set; }
+    }
+
+    public class USD
+    {
+        public string code { get; set; }
+        public string symbol { get; set; }
+        public string rate { get; set; }
+        public string description { get; set; }
+        public double rate_float { get; set; }
+    }
     internal static class Program
     {
+        static HttpClient httpclient = new HttpClient();
+        static async Task Request()
+        {
+
+            string str;
+            using HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Get, "https://api.coindesk.com/v1/bpi/currentprice.json");
+            using HttpResponseMessage response = await httpclient.SendAsync(request);
+            str = await response.Content.ReadAsStringAsync();
+            Root jsonstr = JsonSerializer.Deserialize<Root>(str)!;
+
+            
+            MessageBox.Show(jsonstr.time.updated);
+        }
         /// <summary>
         ///  The main entry point for the application.
         /// </summary>
         [STAThread]
-        static void Main()
+        static async Task Main()
         {
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
             Application.Run(new Form1());
+            await Request();
         }
     }
+
 }
